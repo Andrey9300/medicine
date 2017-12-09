303e8cc6ef73f9f2bb26c83f1109435b48cc9ff4
Andrey <andrey93077@gmail.com>
2017-12-09 09:06:39 +0300

    обновление react и react-* на последние версии, кроме react-router - обновление в рамках мажорной версии. обновление redux и redux-* на последние версии.

--- resources/assets/js/store.js
+++ resources/assets/js/store.js
@@ -1,12 +1,10 @@
 import {applyMiddleware, createStore} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
-import createLogger from 'redux-logger';
+import logger from 'redux-logger';
 import promise from 'redux-promise';
 import reducer from './reducers';
 import thunk from 'redux-thunk';
 import {redirect} from './middlewares/redirect';
-
-const logger = createLogger();
 
 export default createStore(reducer, composeWithDevTools(
     applyMiddleware(thunk, promise, logger, redirect),
