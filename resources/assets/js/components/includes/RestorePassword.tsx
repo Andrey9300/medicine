6f12ca9ac5d5d626c0118ca2a3c546b98184d1cf
andrey93077 <andrey93077@gmail.com>
2020-07-26 17:00:10 +0300

    update package.json

--- resources/assets/js/components/includes/RestorePassword.tsx
+++ resources/assets/js/components/includes/RestorePassword.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
-import PropTypes from 'prop-types';
 import axios from 'axios';
 import {
   Container,
